2b6c7ca314464f22c4874edd2a2c0db3cde17db4
Aleyna06 <celikaleyna71@gmail.com>
2022-08-09 09:52:44 +0300

    Bölüm 7 sonu

---
+++ TravelTripProject/Models/Siniflar/Adres.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace TravelTripProject.Models.Siniflar
+{
+    public class Adres
+    {
+        [Key]
+        public int ID { get; set; }
+        public int Baslik { get; set; }
+        public int Aciklama { get; set; }
+        public int AdresAAcik { get; set; }
+        public int Mail { get; set; }
+        public int Telefon { get; set; }
+        public int Konum { get; set; }
+    }
+}
