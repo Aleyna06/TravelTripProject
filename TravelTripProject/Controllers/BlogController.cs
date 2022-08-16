582e5f6adeec0662a4c8b70722b9cc4b1939e81d
Aleyna06 <celikaleyna71@gmail.com>
2022-08-16 17:18:45 +0300

    Blog Listesi

--- TravelTripProject/Controllers/BlogController.cs
+++ TravelTripProject/Controllers/BlogController.cs
@@ -3,14 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+using TravelTripProject.Models.Siniflar;
 namespace TravelTripProject.Controllers
 {
     public class BlogController : Controller
     {
         // GET: Blog
+        Context c = new Context();
         public ActionResult Index()
         {
+            var bloglar = c.Blogs.ToList();
             return View();
         }
     }
